1553938f24fa1ad2cfc338325a52332854fedaf8
Aleksey Razbakov <ar@kirchbergerknorr.de>
2014-09-16 10:25:38 +0200

    PWASUPP-7 initial commit

---
+++ app/code/community/Kirchbergerknorr/FactFinderSync/Model/Sync.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Sync Model
+ *
+ * @category    Kirchbergerknorr
+ * @package     Kirchbergerknorr
+ * @author      Aleksey Razbakov <ar@kirchbergerknorr.de>
+ * @copyright   Copyright (c) 2014 kirchbergerknorr GmbH (http://www.kirchbergerknorr.de)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Kirchbergerknorr_FactFinderSync_Model_Sync
+{
+    public function start()
+    {
+        $limit = Mage::getStoreConfig('core/factfindersync/queue');
+
+        $this->log("========================================");
+        $timeStart = microtime(true);
+        $this->log("Started FactFinderSync");
+//        $this->resetNewProducts();
+        $this->insertNewProducts();
+        $this->updateImportedProducts();
+        $timeEnd = microtime(true);
+        $time = $timeEnd - $timeStart;
+        $this->log("Finished FactFinderSync limit %s in %s seconds", $limit, $time);
+    }
+
+    public function log($message, $p1 = null, $p2 = null) {
+        if(Mage::getStoreConfig('core/factfindersync/log')) {
+            Mage::log(sprintf($message, $p1, $p2), null, 'kk_factfindersync.log');
+        }
+    }
+
+    protected function resetNewProducts()
+    {
+        $this->log("Reseting products...");
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToFilter('factfinder_created', array('gt' => '0'), 'left');
+
+        foreach ($collection as $product) {
+            $product->setData('factfinder_created', '0');
+            $product->save();
+            $this->log("Resetted #%s", $product->getId());
+        }
+    }
+
+    protected function insertNewProducts()
+    {
+        $this->log("Inserting products...");
+        $limit = Mage::getStoreConfig('core/factfindersync/queue');
+
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('factfinder_created', array('null' => true), 'left')
+            ->setPageSize($limit);
+
+        $factfinder = Mage::getModel('factfindersync/factfinder');
+        $count = $factfinder->setCollection($collection);
+        try {
+            $factfinder->insertProducts();
+            $this->log("Finished import for %s products: %s", $count, $factfinder->getIds());
+        } catch (Exception $e) {
+            $this->log("Error importing: %s", $e->getMessage());
+        }
+    }
+
+    protected function updateImportedProducts()
+    {
+        $this->log("Updating products...");
+        $limit = Mage::getStoreConfig('core/factfindersync/queue');
+
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToSelect('pim_imported')
+            ->addAttributeToFilter('factfinder_updated', array('lt' => 'pim_imported'))
+            ->addAttributeToFilter('pim_imported', array('gt' => '0'), 'left')
+            ->setPageSize($limit);
+
+        $factfinder = Mage::getModel('factfindersync/factfinder');
+        $count = $factfinder->setCollection($collection);
+        try {
+            $factfinder->updateProducts();
+            $this->log("Finished update for %s products: %s", $count,  $factfinder->getIds());
+        } catch (Exception $e) {
+            $this->log("Error importing: %s", $e->getMessage());
+        }
+    }
+}
---
+++ app/code/community/Kirchbergerknorr/FactFinderSync/sql/kk_factfindersync/mysql4-install-0.1.0.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Installer
+ *
+ * @category    Kirchbergerknorr
+ * @package     Kirchbergerknorr
+ * @author      Aleksey Razbakov <ar@kirchbergerknorr.de>
+ * @copyright   Copyright (c) 2014 kirchbergerknorr GmbH (http://www.kirchbergerknorr.de)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/* @var $installer Mage_Sales_Model_Entity_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$attribute  = array(
+    'type'              => 'datetime',
+    'backend'           => '',
+    'frontend'          => '',
+    'label'             => 'FactFinder Created Date',
+    'input'             => 'text',
+    'class'             => '',
+    'source'            => '',
+    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible'           => false,
+    'required'          => false,
+    'user_defined'      => false,
+    'default'           => '',
+    'searchable'        => false,
+    'filterable'        => false,
+    'comparable'        => false,
+    'visible_on_front'  => false,
+    'unique'            => false,
+    'apply_to'          => '',
+    'is_configurable'   => false
+);
+
+$installer->addAttribute('catalog_product', 'factfinder_created', $attribute);
+
+$attribute  = array(
+    'type'              => 'datetime',
+    'backend'           => '',
+    'frontend'          => '',
+    'label'             => 'FactFinder Updated Date',
+    'input'             => 'text',
+    'class'             => '',
+    'source'            => '',
+    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible'           => false,
+    'required'          => false,
+    'user_defined'      => false,
+    'default'           => '',
+    'searchable'        => false,
+    'filterable'        => false,
+    'comparable'        => false,
+    'visible_on_front'  => false,
+    'unique'            => false,
+    'apply_to'          => '',
+    'is_configurable'   => false
+);
+
+$installer->addAttribute('catalog_product', 'factfinder_updated', $attribute);
+
+$attribute  = array(
+    'type'              => 'datetime',
+    'backend'           => '',
+    'frontend'          => '',
+    'label'             => 'PIM Imported Date',
+    'input'             => 'text',
+    'class'             => '',
+    'source'            => '',
+    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible'           => false,
+    'required'          => false,
+    'user_defined'      => false,
+    'default'           => '',
+    'searchable'        => false,
+    'filterable'        => false,
+    'comparable'        => false,
+    'visible_on_front'  => false,
+    'unique'            => false,
+    'apply_to'          => '',
+    'is_configurable'   => false
+);
+
+$installer->addAttribute('catalog_product', 'pim_imported', $attribute);
+
+$installer->endSetup();
