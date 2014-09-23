95a1c6a2636c6a28d49d5974c171ff23000fff97
Aleksey Razbakov <ar@kirchbergerknorr.de>
2014-09-23 14:18:58 +0200

    PWA-7 #comment Fixed job lock

--- app/code/community/Kirchbergerknorr/FactFinderSync/Model/Observer.php
+++ app/code/community/Kirchbergerknorr/FactFinderSync/Model/Observer.php
@@ -20,9 +20,12 @@ class Kirchbergerknorr_FactFinderSync_Model_Observer
 
         if(!Mage::getStoreConfig('core/factfindersync/running'))
         {
-            Mage::getModel('core/config')->saveConfig('core/factfindersync/running', 1);
+            $config = new Mage_Core_Model_Config();
+            $config->saveConfig('core/factfindersync/running', "1", 'default', 0);
+
             Mage::getModel('factfindersync/sync')->start();
-            Mage::getModel('core/config')->saveConfig('core/factfindersync/running', 0);
+
+            $config->saveConfig('core/factfindersync/running', "0", 'default', 0);
         } else {
             Mage::getModel('factfindersync/sync')->log('FactFinderSync is already running');
         }
